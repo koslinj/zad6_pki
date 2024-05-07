e73dbc6154b1cfcb169ba63318db44bb4c2fe463
koslinj <koslinj@gmail.com>
2024-05-07 17:02:25 +0200

    display logged user (etap 2)

--- index.js
+++ index.js
@@ -12,35 +12,46 @@ const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_U
 var authed = false;
 
 app.get('/', (req, res) => {
-    if (!authed) {
-        // Generate an OAuth URL and redirect there
-        const url = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: 'https://www.googleapis.com/auth/gmail.readonly'
-        });
-        console.log(url)
-        res.redirect(url);
-    } else {
-        res.send('Logged in')
-    }
+  if (!authed) {
+    // Generate an OAuth URL and redirect there
+    const url = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/userinfo.profile'
+    });
+    console.log(url)
+    res.redirect(url);
+  } else {
+    var loggedUser
+    var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' })
+    oauth2.userinfo.v2.me.get(function (err, result) {
+      if (err) {
+        console.log('BLAD!')
+        console.log(err)
+      } else {
+        loggedUser = result.data.name
+        console.log(loggedUser)
+      }
+      res.send('Zalogowany: '.concat(loggedUser, '<img src="', result.data.picture, '"height="23" width="23">'))
+    })
+  }
 })
 
 app.get('/auth/google/callback', function (req, res) {
-    const code = req.query.code
-    if (code) {
-        // Get an access token based on our OAuth code
-        oAuth2Client.getToken(code, function (err, tokens) {
-            if (err) {
-                console.log('Error authenticating')
-                console.log(err);
-            } else {
-                console.log('Successfully authenticated');
-                oAuth2Client.setCredentials(tokens);
-                authed = true;
-                res.redirect('/')
-            }
-        });
-    }
+  const code = req.query.code
+  if (code) {
+    // Get an access token based on our OAuth code
+    oAuth2Client.getToken(code, function (err, tokens) {
+      if (err) {
+        console.log('Error authenticating')
+        console.log(err);
+      } else {
+        console.log('Successfully authenticated');
+        oAuth2Client.setCredentials(tokens);
+        authed = true;
+        res.redirect('/')
+      }
+    });
+  }
 });
 
 const port = process.env.PORT || 3001;
